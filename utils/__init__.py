12d632e525aff951f2c6a8eccf8ecdb1557e19c3
mingli <ming.li@gbst.com>
2021-07-16 13:18:02 +1000

    update

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 import os
+from utils import gmail_utils
 PACK_DIR = os.path.dirname(os.path.abspath(__file__))
+# __package__ = 'utils'
 
 
-if __name__ == '__main__':
-    pass
+# if __name__ == '__main__':
+#     pass
